02644a59d74a46b888e6ad1bd46c85fc710c0189
TSUKUMO Akito <tsukumoakito99@duck.com>
2026-09-06 10:23:21 +0900

    chore: release v1.0.7

--- src/core/cli.zig
+++ src/core/cli.zig
@@ -7,7 +7,7 @@ pub const CliResult = struct {
 
 pub fn parse(allocator: std.mem.Allocator) !CliResult {
     var debug_mode = false;
-    var config_path: []const u8 = "/etc/net-bastion/config.json";
+    var config_path: []const u8 = "/etc/circuit-mesh/config.json";
     var config_explicit = false;
 
     const args = try std.process.argsAlloc(allocator);
@@ -27,7 +27,7 @@ pub fn parse(allocator: std.mem.Allocator) !CliResult {
     if (!config_explicit) {
         if (std.process.getEnvVarOwned(allocator, "HOME")) |home| {
             defer allocator.free(home);
-            const user_cfg = try std.fs.path.join(allocator, &[_][]const u8{ home, ".config", "net-bastion", "config.json" });
+            const user_cfg = try std.fs.path.join(allocator, &[_][]const u8{ home, ".config", "circuit-mesh", "config.json" });
             if (std.fs.accessAbsolute(user_cfg, .{})) |_| {
                 config_path = try allocator.dupe(u8, user_cfg);
             } else |_| {
